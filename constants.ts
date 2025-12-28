75b8ea87b149576c643aae31acf7571cad0333d4
Vesa Perasto <vesa.perasto@gmail.com>
2025-12-28 13:45:26 +0200

    fix: Remove duplicate song entry
    The song 'Freedom! ’90' by George Michael was present twice in the constants list. This commit removes the duplicate entry.

--- constants.ts
+++ constants.ts
@@ -13,22 +13,17 @@ export const SONGS: Song[] = [
   { id: '90s-7', artist: 'Backstreet Boys', title: 'I Want It That Way', year: 1999, youtubeId: '4fndeDfaWCg', startAt: 0 },
   { id: '90s-8', artist: 'Spice Girls', title: 'Wannabe', year: 1996, youtubeId: 'gJLIiF15wjQ', startAt: 0 },
   { id: '90s-9', artist: 'Madonna', title: 'Vogue', year: 1990, youtubeId: 'GuJQSAiODqI', startAt: 0 },
-  { id: '90s-10', artist: 'George Michael', title: 'Freedom! ’90', year: 1990, youtubeId: 'diYAc7gB-0M', startAt: 0 },
   { id: '90s-11', artist: 'Metallica', title: 'Enter Sandman', year: 1991, youtubeId: 'CD-E-LDc384', startAt: 0 },
   { id: '90s-12', artist: 'Guns N’ Roses', title: 'November Rain', year: 1991, youtubeId: '8SbUC-UaAxE', startAt: 0 },
   { id: '90s-13', artist: 'Red Hot Chili Peppers', title: 'Under the Bridge', year: 1992, youtubeId: 'GLvohMXgcBo', startAt: 0 },
   { id: '90s-14', artist: 'Aerosmith', title: 'I Don’t Want to Miss a Thing', year: 1998, youtubeId: 'JkK8g6FMEXE', startAt: 0 },
   { id: '90s-15', artist: 'Bon Jovi', title: 'Always', year: 1994, youtubeId: '9BMwcO6_hyA', startAt: 0 },
-  { id: '90s-16', artist: '2Pac', title: 'California Love', year: 1995, youtubeId: 'omHzO-iN6b4', startAt: 0 }, // Changed to Topic Audio
   { id: '90s-17', artist: 'The Notorious B.I.G.', title: 'Juicy', year: 1994, youtubeId: '_JZom_gVfuw', startAt: 0 },
   { id: '90s-18', artist: 'Dr. Dre ft. Snoop Dogg', title: 'Still D.R.E.', year: 1999, youtubeId: '_CL6n0FJZpk', startAt: 0 },
   { id: '90s-19', artist: 'TLC', title: 'No Scrubs', year: 1999, youtubeId: 'FrLequ6dUdM', startAt: 0 },
   { id: '90s-20', artist: 'Coolio', title: 'Gangsta’s Paradise', year: 1995, youtubeId: 'fPO76Jlnz6c', startAt: 0 },
   { id: '90s-21', artist: 'Haddaway', title: 'What Is Love', year: 1993, youtubeId: 'HEXWRTEbj1I', startAt: 0 },
   { id: '90s-22', artist: 'Eiffel 65', title: 'Blue (Da Ba Dee)', year: 1999, youtubeId: '68ugkg9RePc', startAt: 0 },
-  { id: '90s-23', artist: 'Corona', title: 'The Rhythm of the Night', year: 1993, youtubeId: 'u3ltZmI5LQw', startAt: 0 },
-  { id: '90s-24', artist: 'Snap!', title: 'Rhythm Is a Dancer', year: 1992, youtubeId: 'KkhGkRahU6g', startAt: 0 },
-  { id: '90s-25', artist: 'Ace of Base', title: 'All That She Wants', year: 1992, youtubeId: 'd73tiBBebHg', startAt: 0 },
   { id: '90s-26', artist: 'Oasis', title: 'Wonderwall', year: 1995, youtubeId: '6hzrDeceEKc', startAt: 0 },
   { id: '90s-27', artist: 'MC Hammer', title: 'U Can’t Touch This', year: 1990, youtubeId: 'otCpCn0l4Wo', startAt: 0 },
   { id: '90s-28', artist: 'Ricky Martin', title: 'Livin’ La Vida Loca', year: 1999, youtubeId: 'p47fEXGabaY', startAt: 0 },
@@ -59,13 +54,10 @@ export const SONGS: Song[] = [
   { id: '80s-21', artist: 'Prince', title: 'When Doves Cry', year: 1984, youtubeId: 'UG3VcCAlUgE', startAt: 0 },
   { id: '80s-22', artist: 'Madonna', title: 'Material Girl', year: 1985, youtubeId: '6p-lDYPR2P8', startAt: 0 },
   { id: '80s-23', artist: 'Depeche Mode', title: 'Just Can’t Get Enough', year: 1981, youtubeId: '_6FBfAQ-NDE', startAt: 0 },
-  { id: '80s-24', artist: 'The Cure', title: 'Just Like Heaven', year: 1987, youtubeId: 'nmDG0dBj3uQ', startAt: 0 },
   { id: '80s-25', artist: 'AC/DC', title: 'Back in Black', year: 1980, youtubeId: 'pAgnJDJN4VA', startAt: 0 },
   { id: '80s-26', artist: 'Ray Parker Jr.', title: 'Ghostbusters', year: 1984, youtubeId: 'Fe93CLbHjxQ', startAt: 0 },
   { id: '80s-27', artist: 'Wham!', title: 'Wake Me Up Before You Go-Go', year: 1984, youtubeId: 'pIgZ7gMze7A', startAt: 0 },
   { id: '80s-28', artist: 'Phil Collins', title: 'In the Air Tonight', year: 1981, youtubeId: 'YkADj0TPrJA', startAt: 180 },
-  { id: '80s-29', artist: 'Kenny Loggins', title: 'Footloose', year: 1984, youtubeId: 'ltrZd3veNCs', startAt: 0 },
-  { id: '80s-30', artist: 'Soft Cell', title: 'Tainted Love', year: 1981, youtubeId: 'XZVZn3-sfcU', startAt: 0 },
 
   // --- 70-LUKU ---
   { id: '70s-1', artist: 'Earth, Wind & Fire', title: 'September', year: 1978, youtubeId: 'Gs069dndIYk', startAt: 0 },
@@ -74,26 +66,17 @@ export const SONGS: Song[] = [
   { id: '70s-4', artist: 'Stevie Wonder', title: 'Superstition', year: 1972, youtubeId: '0CFuCYNx-1g', startAt: 0 },
   { id: '70s-5', artist: 'Queen', title: 'Bohemian Rhapsody', year: 1975, youtubeId: 'fJ9rUzIMcZQ', startAt: 0 },
   { id: '70s-6', artist: 'Village People', title: 'Y.M.C.A.', year: 1978, youtubeId: 'CS9OO0S5w2k', startAt: 0 },
-  { id: '70s-7', artist: 'Gloria Gaynor', title: 'I Will Survive', year: 1978, youtubeId: 'ARt9HV9T0w8', startAt: 0 },
   { id: '70s-8', artist: 'Fleetwood Mac', title: 'Go Your Own Way', year: 1977, youtubeId: '6ul-cZyuYq4', startAt: 0 },
   { id: '70s-11', artist: 'Led Zeppelin', title: 'Stairway to Heaven', year: 1971, youtubeId: 'QkF3oxziUI4', startAt: 0 },
   { id: '70s-12', artist: 'Elton John', title: 'Rocket Man', year: 1972, youtubeId: 'DtVBCG6ThDk', startAt: 0 },
-  { id: '70s-13', artist: 'Deep Purple', title: 'Smoke on the Water', year: 1972, youtubeId: 'zUwEIt9ez7M', startAt: 0 },
-  { id: '70s-14', artist: 'Eagles', title: 'Hotel California', year: 1976, youtubeId: 'EqPtz5qN7HM', startAt: 0 },
   { id: '70s-15', artist: 'Pink Floyd', title: 'Another Brick in the Wall (Pt. 2)', year: 1979, youtubeId: 'HrxX9TBj2zY', startAt: 0 },
   { id: '70s-16', artist: 'The Jackson 5', title: 'ABC', year: 1970, youtubeId: 'ho7796-au8U', startAt: 0 },
-  { id: '70s-17', artist: 'David Bowie', title: 'Heroes', year: 1977, youtubeId: '44FSlstp6uY', startAt: 0 },
-  { id: '70s-18', artist: 'KC & The Sunshine Band', title: 'That’s the Way (I Like It)', year: 1975, youtubeId: 'R3k6vXQn9r8', startAt: 0 },
   { id: '70s-19', artist: 'Lynyrd Skynyrd', title: 'Free Bird', year: 1973, youtubeId: 'QxIWDmmqZzY', startAt: 0 },
   { id: '70s-21', artist: 'Ramones', title: 'Blitzkrieg Bop', year: 1976, youtubeId: 'iymtpePP8I8', startAt: 0 },
   { id: '70s-22', artist: 'Dolly Parton', title: 'Jolene', year: 1973, youtubeId: 'Ixrje2rXLMA', startAt: 0 },
-  { id: '70s-23', artist: 'Boney M.', title: 'Rasputin', year: 1978, youtubeId: 'kvDMlk3kSYg', startAt: 0 },
-  { id: '70s-24', artist: 'Kiss', title: 'I Was Made For Lovin’ You', year: 1979, youtubeId: 'ZhIsAZO5e0s', startAt: 0 },
   { id: '70s-25', artist: 'John Lennon', title: 'Imagine', year: 1971, youtubeId: 'YkgkThdzX-8', startAt: 0 },
   { id: '70s-26', artist: 'Blondie', title: 'Heart of Glass', year: 1978, youtubeId: 'WGU_4-5RaxU', startAt: 0 },
   { id: '70s-27', artist: 'Black Sabbath', title: 'Paranoid', year: 1970, youtubeId: '0qanF-91aJo', startAt: 0 },
-  { id: '70s-28', artist: 'Sex Pistols', title: 'Anarchy in the UK', year: 1976, youtubeId: 'kB7SCSZebCc', startAt: 0 },
-  { id: '70s-29', artist: 'Kate Bush', title: 'Wuthering Heights', year: 1978, youtubeId: 'Fk-4lXq_Fh0', startAt: 0 },
   { id: '70s-30', artist: 'Meat Loaf', title: 'Bat Out of Hell', year: 1977, youtubeId: '3QGMCSCFoKA', startAt: 0 },
   { id: '70s-31', artist: 'Chic', title: 'Le Freak', year: 1978, youtubeId: 'aXgSHL7efKg', startAt: 0 },
   { id: '70s-32', artist: 'The Clash', title: 'London Calling', year: 1979, youtubeId: 'EfK-WX2pa8c', startAt: 0 },
@@ -113,34 +96,23 @@ export const SONGS: Song[] = [
   { id: '60s-12', artist: 'Bob Dylan', title: 'Like a Rolling Stone', year: 1965, youtubeId: 'IwOfCgkyEj0', startAt: 0 },
   { id: '60s-13', artist: 'The Beatles', title: 'Hey Jude', year: 1968, youtubeId: 'A_MjCqQoLLA', startAt: 0 },
   { id: '60s-14', artist: 'The Doors', title: 'Light My Fire', year: 1967, youtubeId: 'mbj1RFaoyLk', startAt: 0 },
-  { id: '60s-15', artist: 'The Who', title: 'My Generation', year: 1965, youtubeId: 'qEXoYNqIvvI', startAt: 0 },
-  { id: '60s-17', artist: 'Simon & Garfunkel', title: 'The Sound of Silence', year: 1965, youtubeId: '4zLfCnGVeL4', startAt: 0 },
   { id: '60s-18', artist: 'Van Morrison', title: 'Brown Eyed Girl', year: 1967, youtubeId: 'UfmkgQRmmeE', startAt: 0 },
   { id: '60s-19', artist: 'Otis Redding', title: '(Sittin’ On) The Dock of the Bay', year: 1968, youtubeId: 'rTVjnBo96Ug', startAt: 0 },
-  { id: '60s-20', artist: 'The Byrds', title: 'Mr. Tambourine Man', year: 1965, youtubeId: '3t4T4Aln0Cc', startAt: 0 },
   { id: '60s-21', artist: 'The Supremes', title: 'Baby Love', year: 1964, youtubeId: '9_y6nFjoVp4', startAt: 0 },
-  { id: '60s-22', artist: 'Roy Orbison', title: 'Oh, Pretty Woman', year: 1964, youtubeId: 'lnRs3W_i7x4', startAt: 0 },
-  { id: '60s-23', artist: 'Ray Charles', title: 'Hit the Road Jack', year: 1961, youtubeId: 'SrsP8wT9G4E', startAt: 0 },
   { id: '60s-24', artist: 'Nancy Sinatra', title: 'These Boots Are Made for Walkin’', year: 1966, youtubeId: 'SbyAZQ45uww', startAt: 0 },
-  { id: '60s-25', artist: 'The Beatles', title: 'Twist and Shout', year: 1963, youtubeId: '2R-C3nS6Ro0', startAt: 0 },
-  { id: '60s-26', artist: 'Dick Dale', title: 'Misirlou', year: 1962, youtubeId: '-y3h9p_c5-M', startAt: 0 },
   { id: '60s-27', artist: 'Neil Diamond', title: 'Sweet Caroline', year: 1969, youtubeId: '1vhFnTjia_I', startAt: 60 },
-  { id: '60s-28', artist: 'Steppenwolf', title: 'Born to Be Wild', year: 1968, youtubeId: '5UWRyjBEZGA', startAt: 0 },
   { id: '60s-29', artist: 'Marvin Gaye', title: 'I Heard It Through the Grapevine', year: 1968, youtubeId: 'hajBdDM2qdg', startAt: 0 },
   { id: '60s-30', artist: 'Jefferson Airplane', title: 'White Rabbit', year: 1967, youtubeId: 'WANNqr-vcx0', startAt: 0 },
-  { id: '60s-31', artist: 'The Monkees', title: 'I’m a Believer', year: 1966, youtubeId: 'wB9YIsKIEbA', startAt: 0 },
   { id: '60s-32', artist: 'Led Zeppelin', title: 'Whole Lotta Love', year: 1969, youtubeId: 'HQmmM_qwG4k', startAt: 0 },
 ];
 
 export const FINNISH_SONGS: Song[] = [
   // --- MODERNIT & 2000-LUKU ---
-  { id: 'fi-1', artist: 'Käärijä', title: 'Cha Cha Cha', year: 2023, youtubeId: 'rJHe-iZUFf8', startAt: 0 }, // UMK Official Live
   { id: 'fi-2', artist: 'Lordi', title: 'Hard Rock Hallelujah', year: 2006, youtubeId: 'gAh9NRGNhUU', startAt: 0 }, // Eurovision Official Live
   { id: 'fi-3', artist: 'Antti Tuisku', title: 'Peto on irti', year: 2015, youtubeId: 'tk5IW-pYZk8', startAt: 0 }, // Official Audio
   { id: 'fi-4', artist: 'PMMP', title: 'Rusketusraidat', year: 2003, youtubeId: 'b1zKr1kVSlM', startAt: 0 }, // Official Audio
   { id: 'fi-5', artist: 'Maija Vilkkumaa', title: 'Satumaa-tango', year: 1999, youtubeId: 'tleJh6Y4FS4', startAt: 0 }, // Official Audio
   { id: 'fi-6', artist: 'Robin', title: 'Frontside Ollie', year: 2012, youtubeId: 'JPCRtMjCGu4', startAt: 0 }, // Official Music Video
-  { id: 'fi-7', artist: 'Cheek', title: 'Timantit on ikuisia', year: 2013, youtubeId: 'v1YjcZ8KznM', startAt: 0 }, // Official Audio
   { id: 'fi-8', artist: 'Jenni Vartiainen', title: 'Missä muruseni on', year: 2010, youtubeId: 'EUYD8EnVjcc', startAt: 0 }, // Official Audio
   { id: 'fi-9', artist: 'Haloo Helsinki!', title: 'Maailman toisella puolen', year: 2011, youtubeId: 'Hx-F8QlU5pQ', startAt: 0 }, // Official Music Video
   { id: 'fi-10', artist: 'JVG', title: 'Ikuinen vappu', year: 2019, youtubeId: 'hUnd1E2-un8', startAt: 0 }, // Official Music Video
